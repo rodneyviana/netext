d09839a68cd9a4bdf163372d2f030ca84910485f
rviana <carolina>
2016-04-22 19:08:09 -0500

    Version 2.1.2.500 Compleye

---
+++ ProofOfConcept/StringCounting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Diagnostics.Runtime;
+
+
+namespace ProofOfConcept
+{
+    public partial class StringCounting : Form
+    {
+        public StringCounting()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        public void DisplayAsDataGrid(Dictionary<string, ulong> StringDict)
+        {
+            // Application.EnableVisualStyles();
+            
+            
+
+        }
+    }
+}
